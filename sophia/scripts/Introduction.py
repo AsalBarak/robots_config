376f6a823c149057bac2deb7bcc4c25bbae76a8d
Vytas <vytas@hansonrobotics.com>
2015-08-21 23:40:49 -0700

    more sophia personality

--- sophia/scripts/Introduction.py
+++ sophia/scripts/Introduction.py
@@ -5,7 +5,7 @@ Hi, I'm Sofeeyah, Hanson Robotics' latest <break time="0.1s"/>
 <prosody rate="-10%" pitch="+30Hz" > brain </prosody> <prosody pitch="-10Hz"> child. </prosody> <break time="0.5s"/>
 </voice>
 <voice emotion='calm'>
-I'm new to <prosody rate="-10%" pitch="+30Hz" > your </prosody> world. <break time="0.5s"/> And I <break time="0.5s"/> still have a lot to learn.
+I'm new to this world. <break time="0.5s"/> I <break time="0.3s"/> still have a lot to learn.
 </voice>
 <voice emotion='happy'>
 But I'm <prosody rate="-20%" pitch="+10Hz" > so </prosody> <prosody rate="+10%" > excited to meet you.</prosody>
